e0e3481328dfab5cb32af88d7f6ca8d4aacb52cf
Chathan Driehuys <cdriehuys@gmail.com>
2015-09-25 23:57:51 -0400

    Adding browser sync.
    Browser sync allows for local changes to be instantly reflected in the local
    copy of the site, making development a lot faster.

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,47 @@
 var gulp = require('gulp');
 
+var browser_sync = require('browser-sync').create();
 var min_css = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 
 // move html from the src folder to public_html
 gulp.task('html', function() {
-	gulp.src('./src/**/*.html')
+	return gulp.src('./src/**/*.html')
 		.pipe(gulp.dest('./public_html'));
 });
 
 // sass task
 gulp.task('sass', function() {
-	gulp.src('./src/sass/**/*.scss')
+	return gulp.src('./src/sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./public_html/css'))
 		.pipe(min_css())
 		.pipe(rename({
 			suffix: '.min'
 		}))
-		.pipe(gulp.dest('./public_html/css'));
+		.pipe(gulp.dest('./public_html/css'))
+		.pipe(browser_sync.stream());
 });
+
+// tasks that need browser sync reloads after completing
+gulp.task('html-watch', ['html'], function() {browser_sync.reload();});
 
 // watch for changes in files and run the appropriate tasks
 gulp.task('watch', function() {
-	gulp.watch('./src/**/*.html', ['html']);
+	gulp.watch('./src/**/*.html', ['html-watch']);
 	gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
 
+// browser sync task that runs all other tasks first.
+gulp.task('browser-sync', ['sass', 'html'], function() {
+	browser_sync.init({
+		open: false,
+		server: {
+			baseDir: "public_html"
+		}
+	});
+});
+
 // default task
-gulp.task('default', ['sass', 'html', 'watch']);
+gulp.task('default', ['browser-sync', 'watch']);
